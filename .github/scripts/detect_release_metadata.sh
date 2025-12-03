b07c94826d93833add2fa937109cebaf0f5e0eda
Arek Wtykło <arek.wtyklo@arteneo.pl>
2025-12-03 16:42:07 +0100

    Fix paths

--- .github/scripts/detect_release_metadata.sh
+++ .github/scripts/detect_release_metadata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# $1 - Pass "true" when there are changes to `.github/workflows/VERSION` or `.github/workflows/PREVIOUS_VERSION` which indicates a new release
+# $1 - Pass "true" when there are changes to `.github/metadata/VERSION` or `.github/metadata/PREVIOUS_VERSION` which indicates a new release
 
-# Read `.github/workflows/VERSION` and `.github/workflows/PREVIOUS_VERSION` to prepare release metadata as output for GitHub Actions
+# Read `.github/metadata/VERSION` and `.github/metadata/PREVIOUS_VERSION` to prepare release metadata as output for GitHub Actions
 # Prepares the following outputs:
 # - version: The current version
 # - previousVersion: The previous released version
@@ -11,8 +11,8 @@
 # - changesBase: The git SHA or previous version to use as base for change detection
 
 changed="${1}"
-version=$(cat .github/workflows/VERSION)
-previousVersion=$(cat .github/workflows/PREVIOUS_VERSION)
+version=$(cat .github/metadata/VERSION)
+previousVersion=$(cat .github/metadata/PREVIOUS_VERSION)
 
 if [ -z "$version" ] || [ -z "$previousVersion" ]; then
     echo "Failed to read version or previous version"
