a7706d1792d0653f54a447b7264b30613a5a8556
Arek Wtykło <arek.wtyklo@arteneo.pl>
2025-12-03 08:14:48 +0100

    WIP: Github actions

---
+++ .github/scripts/build_docker_image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# $1 - Image name (one of composer, scep, frontend, core, backend, app, test)
+# $2 - Pass "true" when image should be build
+
+image="$1"
+build="$2"
+
+knownImages=("composer" "scep" "frontend" "core" "backend" "app" "test")
+
+if [ -z "$image" ]; then
+    echo "Image name is required"
+    exit 1
+fi
+
+if [[ ! " ${knownImages[*]} " =~ [[:space:]]${image}[[:space:]] ]]; then
+    echo "Unknown image '$image'"
+    exit 1
+fi
+
+if [ "$build" != "true" ]; then
+    echo "Image for '$image' will not be build"
+    exit 0
+fi
+
+echo "Building image for '$image'"
+make help
