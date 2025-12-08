37f3b929e3ed74f4f57a052426dc5d0f8e8c065a
Arek Wtykło <arek.wtyklo@arteneo.pl>
2025-12-08 08:05:05 +0100

    Add fake backend change

--- src/Kernel.php
+++ src/Kernel.php
@@ -21,6 +21,8 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+// TODO Fake change
+
 class Kernel extends BaseKernel implements CompilerPassInterface
 {
     use MicroKernelTrait;
