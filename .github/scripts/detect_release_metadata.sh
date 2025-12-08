e945d518b31c9a07acde13c21bba2fe0c79daa2c
Arek Wtykło <arek.wtyklo@arteneo.pl>
2025-12-08 08:27:00 +0100

    Prepare and test workflow for initial release

--- .github/scripts/detect_release_metadata.sh
+++ .github/scripts/detect_release_metadata.sh
@@ -5,16 +5,16 @@
 # Read `.github/metadata/VERSION` and `.github/metadata/PREVIOUS_VERSION` to prepare release metadata as output for GitHub Actions
 # Prepares the following outputs:
 # - version: The current version
-# - previousVersion: The previous released version
-# - release: "no", "rc" or "production"
-# - changesBase: The git SHA to use as base for change detection. Previous commit when release = "no", or previous version (tag) for release = "rc" or "production"
+# - previousVersion: The previous released version (empty when release = "initial")
+# - release: "no", "rc", "production" or "initial" (without a PREVIOUS_VERSION)
+# - changesBase: The git SHA to use as base for change detection. Previous commit when release = "no" or "initial", or previous version (tag) for release = "rc" or "production"
 
 changed="${1}"
 version=$(cat .github/metadata/VERSION)
 previousVersion=$(cat .github/metadata/PREVIOUS_VERSION)
 
-if [ -z "$version" ] || [ -z "$previousVersion" ]; then
-    echo "Failed to read version or previous version"
+if [ -z "$version" ]; then
+    echo "Failed to read version"
     exit 1
 fi
 
@@ -26,6 +26,11 @@ echo "previousVersion=$previousVersion" >> $GITHUB_OUTPUT
 if [ "$changed" != "true" ]; then
     echo "release=no"
     echo "release=no" >> $GITHUB_OUTPUT
+    echo "changesBase=$GITHUB_REF_NAME"
+    echo "changesBase=$GITHUB_REF_NAME" >> $GITHUB_OUTPUT
+elif  [ -z "$previousVersion" ]; then
+    echo "release=initial"
+    echo "release=initial" >> $GITHUB_OUTPUT
     echo "changesBase=$GITHUB_REF_NAME"
     echo "changesBase=$GITHUB_REF_NAME" >> $GITHUB_OUTPUT
 else
