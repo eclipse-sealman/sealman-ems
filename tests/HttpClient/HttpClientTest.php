0622de55cb7323dac368712c31dab903134a8901
Arek Wtykło <arek.wtyklo@arteneo.pl>
2025-12-17 12:21:45 +0100

    #15 Update composer packages including PHPUnit to v12

--- tests/HttpClient/HttpClientTest.php
+++ tests/HttpClient/HttpClientTest.php
@@ -17,15 +17,14 @@ namespace tests\HttpClient;
 
 use App\HttpClient\HttpClient;
 use App\HttpClient\HttpClientException;
+use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-/**
- * @group smoke
- * @group full
- */
+#[Group('full')]
+#[Group('smoke')]
 class HttpClientTest extends TestCase
 {
     public function testMockResponses()
